e337edc67cf977a06f7bc1fb5c0a78dd28348acb
Rafal <rafaljanko@gmail.com>
2023-01-31 19:53:20 +0100

    db setup and env variables for db

--- PersonalFinanceApplication/wsgi.py
+++ PersonalFinanceApplication/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PersonalFinanceApp project.
+WSGI config for PersonalFinanceApplication project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalFinanceApp.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PersonalFinanceApplication.settings')
 
 application = get_wsgi_application()
