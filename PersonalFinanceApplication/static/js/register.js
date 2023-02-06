36ffec05eccda28b5858bc33fe9a43cdfebf7dc8
Rafal <rafaljanko@gmail.com>
2023-02-06 16:05:58 +0100

    js username validation in register form

---
+++ PersonalFinanceApplication/static/js/register.js
@@ -0,0 +1,28 @@
+const usernameField=document.querySelector('#usernamefield');
+const feedbackArea=document.querySelector('.invalid-feedback')
+
+usernameField.addEventListener('keyup', (e) => {
+    console.log('7777',777777);
+
+    const usernameVal=e.target.value;
+
+    usernameField.classList.remove('is-invalid');
+    feedbackArea.style.display='none';
+    
+    if (usernameVal.length>0) {
+
+        fetch('/authentication/validate-username', {
+            body: JSON.stringify({username: usernameVal}),
+            method: 'POST',
+        })
+            .then((res)=>res.json())
+            .then((data) => {
+                console.log('data',data);
+                if (data.username_error){
+                    usernameField.classList.add('is-invalid');
+                    feedbackArea.style.display='block';
+                    feedbackArea.innerHTML = `<p>${data.username_error}</p>`;
+                }
+        });
+    }
+});
