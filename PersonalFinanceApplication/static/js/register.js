bc3edf66e56cda75435a9d434ec7a25736421a18
Rafal <rafaljanko@gmail.com>
2023-02-07 22:08:51 +0100

    email javascript validation

--- PersonalFinanceApplication/static/js/register.js
+++ PersonalFinanceApplication/static/js/register.js
@@ -1,16 +1,48 @@
 const usernameField=document.querySelector('#usernamefield');
-const feedbackArea=document.querySelector('.invalid-feedback')
+const feedbackArea=document.querySelector('.invalid-feedback');
+const emailField=document.querySelector('#emailField');
+const emailfeedbackArea=document.querySelector('.emailfeedbackArea');
+const usernameSuccessOutput=document.querySelector('.usernameSuccessOutput');
+
+emailField.addEventListener('keyup', (e)=>{
+const emailVal=e.target.value;
+    emailField.style.display='block';
+    emailField.textContent=`Checking ${emailVal}`
+
+
+    emailField.classList.remove('is-invalid');
+    emailfeedbackArea.style.display='none';
+
+    if (emailVal.length>0) {
+        fetch('/authentication/validate-email', {
+            body: JSON.stringify({email: emailVal}),
+            method: 'POST',
+        })
+            .then((res)=>res.json())
+            .then((data) => {
+                console.log('data',data);
+                if (data.email_error){
+                    emailField.classList.add('is-invalid');
+                    emailfeedbackArea.style.display='block';
+                    emailfeedbackArea.innerHTML = `<p>${data.email_error}</p>`;
+                }
+        });
+    }
+
+});
+
 
 usernameField.addEventListener('keyup', (e) => {
-    console.log('7777',777777);
+
 
     const usernameVal=e.target.value;
+    usernameSuccessOutput.style.display='block';
+    usernameSuccessOutput.textContent=`Checking ${usernameVal}`
 
     usernameField.classList.remove('is-invalid');
     feedbackArea.style.display='none';
     
     if (usernameVal.length>0) {
-
         fetch('/authentication/validate-username', {
             body: JSON.stringify({username: usernameVal}),
             method: 'POST',
@@ -18,6 +50,7 @@ usernameField.addEventListener('keyup', (e) => {
             .then((res)=>res.json())
             .then((data) => {
                 console.log('data',data);
+                usernameSuccessOutput.style.display='none';
                 if (data.username_error){
                     usernameField.classList.add('is-invalid');
                     feedbackArea.style.display='block';
