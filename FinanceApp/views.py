ea58c96240d564a83cbd11fe4d28b02ec865a095
Rafal <rafaljanko@gmail.com>
2023-02-22 21:17:31 +0100

    Adding expense functionality

--- FinanceApp/views.py
+++ FinanceApp/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
+from django.contrib import messages
+from django.shortcuts import redirect
 # Create your views here.
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'FinanceApp/index.html', {'categories': categories})
+
+    return render(request, 'FinanceApp/index.html', {'categories':categories})
 
 def add_expense(request):
     categories = Category.objects.all()
-    return render(request, 'FinanceApp/add_expense.html', {'categories': categories})
+    context = {'categories': categories,
+               'values': request.POST}
+    categories = Category.objects.all()
+    if request.method == 'GET':
+        return render(request, 'FinanceApp/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'FinanceApp/add_expense.html', context)
+
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'FinanceApp/add_expense.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date, category=category, description=description, )
+        messages.success(request, 'Expense saved successfully')
+        return redirect('expenses')
+
