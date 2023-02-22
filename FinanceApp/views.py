758506331b8604ca9fa1b526451e49d560676bdf
Rafal <rafaljanko@gmail.com>
2023-02-22 15:14:55 +0100

    setup for expense creation function

--- FinanceApp/views.py
+++ FinanceApp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
 # Create your views here.
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    return render(request, 'FinanceApp/index.html')
+    categories = Category.objects.all()
+    return render(request, 'FinanceApp/index.html', {'categories': categories})
 
 def add_expense(request):
-    return render(request, 'FinanceApp/add_expense.html')
+    categories = Category.objects.all()
+    return render(request, 'FinanceApp/add_expense.html', {'categories': categories})
