e5b96f04d130bc33a8ab0a7d954f615c2eb5a7ed
Rafal <rafaljanko@gmail.com>
2023-03-01 16:06:35 +0100

    setup for ajax search

--- FinanceApp/views.py
+++ FinanceApp/views.py
@@ -4,8 +4,21 @@ from .models import Category, Expense
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
 # Create your views here.
 
+def search_expenses(request):
+    if request.method == "POST":
+        search_string = json.loads(request.body).get('searchText')
+
+        expenses = Expense.objects.filter(amount__istartswith=search_string, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_string, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_string, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_string, owner=request.user)
+
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 @login_required(login_url='/authentication/login')
 def index(request):
