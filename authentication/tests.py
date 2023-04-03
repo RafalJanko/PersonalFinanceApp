1806ef6fcc53647b43894da627518bb6e18838e5
Rafal <rafaljanko@gmail.com>
2023-04-03 19:56:42 +0200

    adding sig in tests

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import authenticate, get_user_model
 
 # Create your tests here.
+
+
+'''
+Tests written for sign-in functionality, testing model fields + relations and generic Create/Update/Delete model items.
+'''
+
+
+class SigninTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='12test12', email='test@example.com')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username='test', password='12test12')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='wrong', password='12test12')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_password(self):
+        user = authenticate(username='test', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
