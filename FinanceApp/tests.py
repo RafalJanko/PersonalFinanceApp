6950287ae03ec50f52a1be29e8febc2f9e27c000
Rafal <rafaljanko@gmail.com>
2023-04-04 21:37:15 +0200

    adding user model tests

--- FinanceApp/tests.py
+++ FinanceApp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from FinanceApp.models import User
+from datetime import datetime
 
 # Create your tests here.
+
+
+class BaseUserModelTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(BaseUserModelTestCase, cls).setUpClass()
+        cls.user = User(first_name="jan", last_name="kowalski", password="asd", email="asd@asd.com")
+        cls.user.save()
+
+
+class UserModelTestCase(BaseUserModelTestCase):
+
+    def test_created_properly(self):
+        self.assertEqual(self.user.first_name, 'jan')
+
+    def test_it_has_information_fields(self):
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+
+    def test_it_has_timestamps(self):
+        self.assertIsInstance(self.user.date_joined, datetime)
