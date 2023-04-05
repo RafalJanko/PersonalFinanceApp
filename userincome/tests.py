5e08e86d0a7ce2e4d353ae8873358a9359732fa3
Rafal <rafaljanko@gmail.com>
2023-04-05 15:43:58 +0200

    Income model tests added

--- userincome/tests.py
+++ userincome/tests.py
@@ -1,5 +1,30 @@
 from django.test import TestCase
+from userincome.models import UserIncome
+from datetime import date, datetime
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
 
+class BaseUserIncomeModelTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(BaseUserIncomeModelTestCase, cls).setUpClass()
+        cls.income = UserIncome(amount=5000, description="test", date=datetime.today(), owner_id=1)
+        cls.income.save()
+        cls.user = User(first_name="jan", last_name="kowalski", password="asd", email="asd@asd.com")
+        cls.user.save()
+
+
+class UserIncomeTestCase(BaseUserIncomeModelTestCase):
+
+    def test_created_properly(self):
+        self.assertEqual(self.income.description, 'test')
+
+    def test_it_has_information_fields(self):
+        self.assertIsInstance(self.income.amount, int)
+        self.assertIsInstance(self.income.description, str)
+
+    def test_it_has_date_fields(self):
+        self.assertIsInstance(self.income.date, datetime)
